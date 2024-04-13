e5e7c317c27751ec0cd04452981cb01515122aa2
hedi72 <heditaieb72@gmail.com>
2024-04-13 16:45:13 +0100

    tasl

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,47 +1,41 @@
+
 #!/usr/bin/python3
+""" The N-queens puzzle"""
+
+
 import sys
 
-def is_safe(board, row, col):
-    # Check if there is a queen in the same column
+
+def is_safe(board, row, col, n):
     for i in range(row):
-        if board[i] == col:
+        if board[i][col] == 1:
             return False
 
-        # Check diagonals
-        if abs(board[i] - col) == abs(i - row):
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, n)):
+        if board[i][j] == 1:
             return False
 
     return True
 
-def solve_n_queens(n):
-    def solve(board, row):
-        if row == n:
-            solutions.append(list(enumerate(board)))
-            return
 
-        for col in range(n):
-            if is_safe(board, row, col):
-                board[row] = col
-                solve(board, row + 1)
-                board[row] = -1
+def solve_nqueens_util(board, row, n, solutions):
+    if row == n:
+        solutions.append([[i, col] for i, col in enumerate(board[row - n])])
+        return
 
-    solutions = []
-    board = [-1] * n
-    solve(board, 0)
-    return solutions
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row][col] = 1
+            solve_nqueens_util(board, row + 1, n, solutions)
+            board[row][col] = 0
 
-def print_solutions(solutions):
-    for solution in solutions:
-        print(solution)
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
+def solve_nqueens(n):
+    if not isinstance(n, int):
         print("N must be a number")
         sys.exit(1)
 
@@ -49,8 +43,24 @@ def main():
         print("N must be at least 4")
         sys.exit(1)
 
-    solutions = solve_n_queens(n)
-    print_solutions(solutions)
+    board = [[0] * n for _ in range(n)]
+    solutions = []
+    solve_nqueens_util(board, 0, n, solutions)
+
+    for solution in solutions:
+        print(solution)
+        print()
+
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    solve_nqueens(N)
