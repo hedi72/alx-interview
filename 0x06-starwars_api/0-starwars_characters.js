aed88340dbb0ae6bac17d0c9a09c6bae40e29994
hedi72 <heditaieb72@gmail.com>
2024-04-19 09:06:27 +0100

    task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,27 +2,58 @@
 
 const request = require("request");
 
-const movieID = process.argv[2];
-const options = { json: true };
-const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+const movieId = process.argv[2];
 
-request(url, options, async (error, response, body) => {
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
   if (error) {
     console.error(error);
     return;
   }
 
-  for (const character of body.characters) {
-    await new Promise((resolve) => {
-      request(character, options, (error, response, body) => {
+  if (response.statusCode !== 200) {
+    console.error(
+      `Failed to fetch movie data. Status code: ${response.statusCode}`
+    );
+    return;
+  }
+
+  const charactersArray = JSON.parse(body).characters;
+
+  function fetchCharacter(characterUrl) {
+    return new Promise((resolve, reject) => {
+      request.get(characterUrl, (error, response, body) => {
         if (error) {
-          console.error(error);
-          resolve();
+          reject(new Error(error));
+          return;
         }
 
-        console.log(body.name);
+        if (response.statusCode !== 200) {
+          reject(
+            new Error(
+              `Failed to fetch character data. Status code: ${response.statusCode}`
+            )
+          );
+          return;
+        }
+
+        const characterName = JSON.parse(body).name;
+        console.log(characterName);
         resolve();
       });
     });
   }
+
+  async function fetchAndPrintCharacters() {
+    for (const characterUrl of charactersArray) {
+      try {
+        await fetchCharacter(characterUrl);
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+  }
+
+  fetchAndPrintCharacters();
 });
