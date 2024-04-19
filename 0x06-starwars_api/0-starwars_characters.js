1cd4f953659dc7b099935a3bcd593e57385dd19b
hedi72 <heditaieb72@gmail.com>
2024-04-19 09:04:11 +0100

    task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,42 +1,29 @@
 
 #!/usr/bin/node
-/**
- * Wrapper function for request object that allows it
- * to work with async and await
- * @param   {String} url - site url
- * @returns {Promise}    - promise object that resolves
- *                         with parsed JSON response
- *                         and rejects with the request error.
- */
-function makeRequest (url) {
-  const request = require('request');
-  return new Promise((resolve, reject) => {
-    request.get(url, (error, response, body) => {
-      if (error) reject(error);
-      else resolve(JSON.parse(body));
-    });
-  });
-}
 
-/**
- * Entry point - makes requests to Star Wars API
- * for movie info based movie ID passed as a CLI parameter.
- * Retrieves movie character info then prints their names
- * in order of appearance in the initial response.
- */
-async function main () {
-  const args = process.argv;
+const request = require('request');
 
-  if (args.length < 3) return;
+const movieID = process.argv[2];
+const options = { json: true };
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
-  const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + args[2];
-  const movie = await makeRequest(movieUrl);
-
-  if (movie.characters === undefined) return;
-  for (const characterUrl of movie.characters) {
-    const character = await makeRequest(characterUrl);
-    console.log(character.name);
+request(url, options, async (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
   }
-}
 
-main();
+  for (const character of body.characters) {
+    await new Promise((resolve) => {
+      request(character, options, (error, response, body) => {
+        if (error) {
+          console.error(error);
+          resolve();
+        }
+
+        console.log(body.name);
+        resolve();
+      });
+    });
+  }
+});
